0e781fd90f87f4ffec9f97ec35b4e32f6ef62138
patrick jesssen <patrick-j1@hotmail.dk>
2021-02-22 09:19:12 +0100

    Add project files.

---
+++ Geometri/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square(5);
+            Console.WriteLine(square.CalculateCircumference());
+            Console.WriteLine(square.CalculateArial());
+            Square square1 = new Square();
+            square1.Side = 40;
+
+            Console.WriteLine(square1.CalculateCircumference());
+            Console.WriteLine(square1.CalculateArial());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Geometri/Square.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri
+{
+    class Square
+    {
+        public int Side { get; set; }
+
+        public Square(int side)
+        {
+            Side = side;
+        }
+
+        public Square()
+        {
+
+        }
+
+        public int CalculateCircumference()
+        {
+            return Side * 4;
+        }
+
+        public int CalculateArial()
+        {
+            return Side * Side;
+        }
+    }
+}
